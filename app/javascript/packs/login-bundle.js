deb3c33faad5970f24f9176f3542c8bb6868be02
reactionic127 <richard.gill127@gmail.com>
2021-05-25 02:42:20 +0800

    changed functional component and removed useless stuff

--- app/javascript/packs/login-bundle.js
+++ app/javascript/packs/login-bundle.js
@@ -1,6 +1,6 @@
 import ReactOnRails from 'react-on-rails';
 
-import Login from '../bundles/Login/Login';
+import { Login } from '../bundles/Login/Login';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
