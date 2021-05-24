43c3ea27b75fe3b967f49cb370439ef43f158285
reactionic127 <richard.gill127@gmail.com>
2021-05-25 04:22:17 +0800

    added projects component

---
+++ app/javascript/bundles/Projects/Projects.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Projects.scss'
+
+export function Projects() {
+    return (
+      <div className="container">
+        <button>Projects</button>
+      </div>
+    );
+}
--- app/javascript/packs/login-bundle.js
+++ app/javascript/packs/login-bundle.js
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import { Login } from '../bundles/Login/Login';
+import { Projects } from '../bundles/Projects/Projects';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   Login,
+  Projects,
 });
