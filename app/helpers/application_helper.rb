368bd40c9c741833de0a41daeac5cf0b150225ce
reactionic127 <richard.gill127@gmail.com>
2021-05-25 04:02:03 +0800

    facebook login/logout is working

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  class TokenVerifier
+    include ActiveSupport::Configurable
+    include ActionController::RequestForgeryProtection
+  
+    def call(env)
+      @request = ActionDispatch::Request.new(env.dup)
+      raise OmniAuth::AuthenticityError unless verified_request?
+    end
+  
+    private
+    attr_reader :request
+    delegate :params, :session, to: :request
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root to: "login#index"
+  devise_scope :user do
+    delete "/users/sign_out" => "devise/sessions#destroy"
+  end
 end
