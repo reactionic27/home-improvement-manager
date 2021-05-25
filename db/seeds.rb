b1b5f50442d94aeab6a503abeacecb9b4eca4aed
reactionic127 <richard.gill127@gmail.com>
2021-05-25 12:13:33 +0800

    seed updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,12 @@
 puts '== Seeding database =='
 
-10.times do |i|
+100.times do |i|
   types = ['private', 'public']
   status = ['created', 'started', 'stopped', 'completed']
   Project.create!(
     name: Faker::Lorem.sentence,
     description: Faker::Lorem.sentence,
-    project_type: types[rand(0...1)],
+    project_type: types.sample,
     estimated_level: rand(20...30),
     actual_level: rand(10...19),
     status: status[rand(0...3)]
